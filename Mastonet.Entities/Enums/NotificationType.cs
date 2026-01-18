9d374ea8c4b132e323417fc633557b0ec4c34ea5
Eduardo Braga <braga.eduardoTL@outlook.com>
2026-01-17 22:49:52 -0300

    Remove commented usings

--- Mastonet.Entities/Enums/NotificationType.cs
+++ Mastonet.Entities/Enums/NotificationType.cs
@@ -1,9 +1,4 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Text.Json;
-//using System.Text.Json.Serialization;
-
-namespace Mastonet.Entities.Enums;
+﻿namespace Mastonet.Entities.Enums;
 
 public enum NotificationType
 {
